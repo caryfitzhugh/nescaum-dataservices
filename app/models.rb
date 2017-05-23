8ccfa8bf1ebecb99e5fd112d59509ef40773afc8
Cary FitzHugh <cary.fitzhugh@gmail.com>
2017-05-23 13:53:43 -0400

    adds more connection debugging

--- app/models.rb
+++ app/models.rb
@@ -2,14 +2,23 @@ require 'app/models/resource'
 require 'lib/config'
 DataMapper.finalize
 
-DataMapper::Logger.new($stdout, :debug)
+DataMapper::Logger.new($stdout, :info)
 
 if CONFIG.postgres
-  puts ENV["RDS_PASSWORD"]
+
   connected = false
   while !connected
     begin
-      DataMapper.setup(:default, CONFIG.postgres)
+      args = {
+        adapter: "postgresql",
+        database: ENV['RDS_DB_NAME'],
+        username: ENV['RDS_USERNAME'],
+        password: ENV['RDS_PASSWORD'],
+        host: ENV["RDS_HOSTNAME"]
+      }
+      require 'pp'; pp args
+      #DataMapper.setup(:default, CONFIG.postgres)
+      DataMapper.setup(:default, args )
       connected = true
     rescue DataObjects::ConnectionError => e
       connected = false
