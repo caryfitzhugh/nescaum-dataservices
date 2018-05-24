b24ccb636cd2c0b123b46fda08abf4288af11180
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-05-24 18:23:08 -0400

    updates for the new ACIS fields

--- cron/acis/ingest_acis.js
+++ cron/acis/ingest_acis.js
@@ -1,7 +1,5 @@
-// npm install pg --save
-const {Client} = require('pg');
 const http = require('http');
-
+const pg = require('pg');
 const HOSTNAME = "grid2.rcc-acis.org";
 const PORT = 80;
 const PATH = "/GridData";
@@ -11,7 +9,7 @@ let db_table = process.argv[2];
 
 
 const get_db_connection = () => {
-  let client = new Client({
+  let client = new pg.Client({
     user: process.env.MAP_RDS_USERNAME,
     host: process.env.MAP_RDS_HOST,
     database: process.env.MAP_RDS_DB,
@@ -75,11 +73,11 @@ const download_acis_data = (opts) => {
                 "reduce":  opts.elem_reduce,
                 "interval": opts.elem_interval
                 },
-            "interval":[20],
-            "duration": 20,
+            "interval": [opts.interval],
+            "duration": opts.duration,
             "reduce": "mean",
-            "sdate": [2020],
-            "edate": [2100],
+            "sdate": opts.sdate,
+            "edate": opts.edate,
             "area_reduce": opts.area_reduce
             }]
       };
@@ -125,14 +123,33 @@ rows_client.query(`SELECT * FROM ${db_table}`, (err, res) => {
     console.error(err.stack);
   } else {
     res.rows.forEach((row) => {
+      // Process to fix up data
+      let elem_interval = [];
+      row.elem_interval.split(",").forEach((v) => {
+        elem_interval.push(parseInt(v, 10));
+      });
+      row.elem_interval = elem_interval;
+
+      let sdate = [];
+      row.sdate.split(",").forEach((v) => {
+        sdate.push(parseInt(v, 10));
+      });
+      row.sdate = sdate;
+
+      let edate = [];
+      row.edate.split(",").forEach((v) => {
+        edate.push(parseInt(v, 10));
+      });
+      row.edate = edate;
+
+      //
       download_acis_data(row)
         .then((data) => {
           const query = {
             text: `UPDATE ${db_table} SET json_blob = $1 WHERE ny_json_pk = $2`,
             values: [data, row.ny_json_pk]
           }
-
-          console.log("Updating...")
+	console.log("Updating...")
           // callback
           let update_client = get_db_connection()
           update_client.query(query, (err, res) => {
