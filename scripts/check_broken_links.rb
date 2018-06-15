9955ff11c03fdcb446361b43d06b7923fa93c5fa
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-06-14 21:07:56 -0400

    scrips for link checking

---
+++ scripts/check_broken_links.rb
@@ -0,0 +1,30 @@
+require './nds_app'
+
+require 'net/http'
+require 'uri'
+
+def valid_path?(path, allowed_redirects=5)
+  allowed_redirects.times do
+    url = URI.parse(path)
+    response = Net::HTTP.new(url.host, url.port).request_head(url.path)
+
+    if response.kind_of?(Net::HTTPRedirection)
+      path = response['location']
+    else
+      return response.kind_of?(Net::HTTPSuccess)
+    end
+  end
+
+  false
+end
+
+Resource.all(indexed: true).each_chunk(100) do |chunk|
+  chunk.each do |resource|
+    resource.external_data_links.each do |edl|
+      url = edl.split("::", 2)[1]
+      if !valid_path(url)
+        puts resource.id
+      end
+    end
+  end
+end
