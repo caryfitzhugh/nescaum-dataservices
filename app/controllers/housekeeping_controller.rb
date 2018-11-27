3ea9d1626570966737dc89b6d65f84ae4ee34e18
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-11-27 16:57:10 -0500

    update syntax

--- app/controllers/housekeeping_controller.rb
+++ app/controllers/housekeeping_controller.rb
@@ -33,9 +33,16 @@ module Controllers
           <<-EMAIL_BODY
             <h2>#{broken_resources.length} Resources Found with Broken Links</h2>
             <ul>
-            #{broken_resources.map do |r|
-              "<li>#{r[0].id} #{r[0].title} - <ul>#{r[1].map {|l| "<li>#{l}</li>")}}</ul> </li>"
-            end}
+              #{broken_resources.map do |r|
+                "<li>#{r[0].id} #{r[0].title}" +
+                  "<ul>" +
+                    r[1].map do |l|
+                      "<li>#{l}</li>"
+                    end.join("")
+                  + "</ul>"+
+                "</li>"
+              end
+              }
             </ul>
           EMAIL_BODY
         end
