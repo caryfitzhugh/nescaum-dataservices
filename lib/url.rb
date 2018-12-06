64ab1b9a220febbc07f76a1a6e87788229b9f5dc
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-12-06 17:06:46 -0500

    updates for chromedriver

--- lib/url.rb
+++ lib/url.rb
@@ -32,6 +32,8 @@ def with_browser()
       options = Selenium::WebDriver::Chrome::Options.new
       options.add_argument('--headless')
       options.add_argument('--disable-gpu')
+      options.add_argument('--no-sandbox')
+      options.add_argument('--disable-dev-shm-usage')
       with_proxy() do |proxy, caps|
         driver = Selenium::WebDriver.for :chrome, options: options, desired_capabilities: caps
         yield driver, proxy
